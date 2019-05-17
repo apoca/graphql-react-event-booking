1af5476d790fcfe9f509d49702cdd30e2951153e
apoca <vieira@esolidar.com>
2019-05-17 17:36:38 +0100

    Second class - Migrating to mongo

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const graphqlHttp = require('express-graphql');
 const {
     buildSchema
 } = require('graphql');
-
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 const app = express();
 
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -43,22 +43,43 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event.find().then(events => {
+                return events.map(event => {
+                    return {
+                        ...event._doc
+                    };
+                });
+            });
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            };
-            console.log(args);
-            events.push(event);
-            return event;
+                date: new Date(args.eventInput.date)
+            });
+
+            return event
+                .save()
+                .then(result => {
+                    console.log(result);
+                    return {
+                        ...result._doc
+                    };
+                }).catch(err => {
+                    console.log(err);
+                    throw err;
+                });
         }
     },
     graphiql: true
 }));
 
-app.listen(9000);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-94ovv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('mongoDB is connected...');
+    app.listen(9000);
+}).catch(err => {
+    console.log(err);
+});
